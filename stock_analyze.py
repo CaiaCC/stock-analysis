83d0adae8b62270c0d6ae6f7d88e9efb8a010e71
Caia Chuang <caiachuang@gmail.com>
2024-01-28 15:21:03 -0800

    used panda.DateOffset instead of dateutil.relativedelta to calculate start date

--- stock_analyze.py
+++ stock_analyze.py
@@ -1,19 +1,20 @@
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import pandas as pd
+import plotly.express as px
 import yfinance as yf
+
 
 # the stocks tickers for analysis
 tickers = ["MSFT", "GOOGL", "AAPL"]
 
 # Get the start and end dates of 3 month time frame
 end_date = datetime.now()
-start_date = datetime.now() + relativedelta(months=-3)
+start_date = end_date - pd.DateOffset()
 
 # get the stock data in the past 3 month
 df_list = []
 for ticker in tickers:
-    data = yf.download(ticker, start=start_date, end=end_date)
+    data = yf.download(tickers, start=start_date, end=end_date)
     df_list.append(data)
 
 df = pd.concat(df_list, keys=tickers, names=['Ticker', 'Date'])
