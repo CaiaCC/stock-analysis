53da9e22b7d7723fa1b5b559ebc1fe03e8122cd5
Caia Chuang <caiachuang@gmail.com>
2024-01-28 16:52:37 -0800

    export data to csv

--- stock_analyze.py
+++ stock_analyze.py
@@ -9,7 +9,7 @@ tickers = ["MSFT", "GOOGL", "AAPL"]
 
 # Get the start and end dates of 3 month time frame
 end_date = datetime.now()
-start_date = end_date - pd.DateOffset()
+start_date = end_date - pd.DateOffset(months=1)
 
 # get the stock data in the past 3 month
 df_list = []
@@ -17,7 +17,10 @@ for ticker in tickers:
     data = yf.download(tickers, start=start_date, end=end_date)
     df_list.append(data)
 
+print("list", df_list)
+
 df = pd.concat(df_list, keys=tickers, names=['Ticker', 'Date'])
 df = df.reset_index()
+df.to_csv('StockData.csv')
 
-print(df.head(10))
+# print("df", df.head(20))
